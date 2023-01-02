2be6f947e3de8fb488ae3b58ef49f0b12b254854
inunekoharu <70610346+inunekoharu@users.noreply.github.com>
2023-01-02 23:28:21 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>〇</title>
+    <style>
+      #contauner{
+        width:306px;
+        heigt:306px;
+        background: glay;
+        margin:20px auto;
+    </style>
+  </head>
+  <body>
+  <div id=container>  
+    </div>
+  </body>
+</html>
