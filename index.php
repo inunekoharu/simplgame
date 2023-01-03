5ce1395ca327a2132c2159682e6a09f36b998b67
inunekoharu <70610346+inunekoharu@users.noreply.github.com>
2023-01-03 11:57:58 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,29 @@
-<!DOTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>〇</title>
+    <title>〇×ゲーム</title>
     <style>
       #contauner{
         width:306px;
         heigt:306px;
-        background: glay;
+        background: black;
         margin:20px auto;
+      }
+      .box { 
+          width:100px;
+          height:100px;
+          border:1px solid red;
+          background: white;
+          float: left;
+      }
     </style>
   </head>
   <body>
-  <div id=container>  
+  <div id="container">
+  <?php for ($x =0; $x<3; $x++) ?>
+    <div class ="box"></div>
     </div>
   </body>
 </html>
+
+
